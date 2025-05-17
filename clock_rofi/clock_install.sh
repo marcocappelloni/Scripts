469254816c6c315a34a23654ddff6c3230dad4a2
marcocappelloni <marco.cappelloni@gmail.com>
2025-05-17 04:05:57 +0100

    Removed the font file from the clock_rofi and added the wget command to download it

--- clock_rofi/clock_install.sh
+++ clock_rofi/clock_install.sh
@@ -6,6 +6,8 @@ ln -s ./clock.sh $HOME/.local/bin/clock
 echo "Do you want to install the Iosevka Nerd Font? (y/n)"
 read response
 if [[ "$response" =~ ^[Yy]$ ]]; then
+  # download the nerd font from "https://www.nerdfonts.com/font-downloads"
+  wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Iosevka.zip"
   mkdir Iosevka
   unzip Iosevka.zip -d Iosevka/
   sudo mv Iosevka/ /usr/share/fonts/truetype/
