bda4cfd1dbd03fdbedfb09138b4b96ea304e930f
marcocappelloni <marco.cappelloni@gmail.com>
2025-05-21 09:59:04 +0100

    Added geany and monitors dimmer script install

---
+++ debian/geany_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Author: Marco Cappelloni
+# Script to install Geany with the main plugins and some colorschemes
+
+# Update and upgrade the system
+sudo apt update && sudo apt upgrade
+
+# Install Geany
+sudo apt install -y geany
+
+# Install the main plugins
+sudo apt install geany-plugin-addons geany-plugin-git-changebar geany-plugin-spellcheck geany-plugin-treebrowser geany-plugin-vimode geany-plugin-markdown
+
+# Destination folder for the color color schemes
+SCHEME_FOLDER="~/.config/geany/colorschemes"
+
+# Create, if it doesn't exist, the directory where to save the colorschemes
+mkdir -p "$SCHEME_FOLDER"
+
+# Download all the themes that I chosen in the correct destination folder
+wget -P "$SCHEME_FOLDER" "https://raw.github.com/geany/geany-themes/master/colorschemes/kary-pro-colors-dark.conf"
+wget -P "$SCHEME_FOLDER" "https://raw.github.com/geany/geany-themes/master/colorschemes/one-dark.conf"
+wget -P "$SCHEME_FOLDER" "https://raw.github.com/geany/geany-themes/master/colorschemes/underthesea.conf"
+wget -P "$SCHEME_FOLDER" "https://raw.github.com/geany/geany-themes/master/colorschemes/zenburn.conf"
+wget -P "$SCHEME_FOLDER" "https://raw.githubusercontent.com/catppuccin/geany/refs/heads/main/src/catppuccin-frappe.conf"
+wget -P "$SCHEME_FOLDER" "https://raw.githubusercontent.com/catppuccin/geany/refs/heads/main/src/catppuccin-macchiato.conf"
+wget -P "$SCHEME_FOLDER" "https://raw.githubusercontent.com/catppuccin/geany/refs/heads/main/src/catppuccin-mocha.conf"
--- dimmer/dimmer_install.sh
+++ dimmer/dimmer_install.sh
@@ -1,7 +1,15 @@
-SHELL=/bin/bash
+#!/bin/bash
 
+# Save the current crontab entries to a variable
+current_crontab=$(crontab -l)
+
+# Append a new cron job to the variable
+new_crontab="${current_crontab}
 0 6 * * * /home/maky/PersonalHome/Scripts/dimmer.sh 1
 0,30 17,18 * * * /home/maky/PersonalHome/Scripts/dimmer.sh 0.9
 0,30 19 * * * /home/maky/PersonalHome/Scripts/dimmer.sh 0.8 night
 0,30 20-23 * * * /home/maky/PersonalHome/Scripts/dimmer.sh 0.7 night
 0,30 0-5 * * * /home/maky/PersonalHome/Scripts/dimmer.sh 0.7 night"
+
+# Update the crontab with the new entries
+echo "$new_crontab" | crontab -
