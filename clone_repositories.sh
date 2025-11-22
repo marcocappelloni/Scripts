0666dae0978063448405e900c48d6de75a1e9319
marcocappelloni <marco.cappelloni@gmail.com>
2025-11-22 01:36:01 +0000

    Fixed script to clone repositories

--- clone_repositories.sh
+++ clone_repositories.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-mkdir -p $HOME/PersonalHome
-echo "Cloning Scripts folder from github"
-git clone https://github.com/marcocappelloni/Scripts.git $HOME/PersonalHome
-
 # Add execute permissions to all the scripts
-find $HOME/PersonalHome/Scripts/ -type f -name "*.sh" -exec chmod u+x {} \;
+#find $HOME/PersonalHome/Scripts/ -type f -name "*.sh" -exec chmod u+x {} \;
 
-mkdir -p $HOME/packages/
+mkdir -p $HOME/packages/suckless
+cd $HOME/packages/suckless
 echo "Cloning suckless folder from github"
-git clone https://github.com/marcocappelloni/suckless.git $HOME/packages
+git clone https://github.com/marcocappelloni/suckless.git
 
+mkdir -p $HOME/dotfiles
+cd $HOME/dotfiles
 echo "Cloning dotfiles folder from github"
-git clone https://github.com/marcocappelloni/dotfiles.git $HOME
+git clone https://github.com/marcocappelloni/dotfiles.git
