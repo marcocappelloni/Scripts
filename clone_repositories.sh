52bc7a69e684cb77cac273d1ff4c7cc037190e25
marcocappelloni <marco.cappelloni@gmail.com>
2025-12-10 23:43:27 +0000

    modified clone_repositories adding checks to see if the directories were cloned already

--- clone_repositories.sh
+++ clone_repositories.sh
@@ -3,11 +3,15 @@
 # Add execute permissions to all the scripts
 #find $HOME/PersonalHome/Scripts/ -type f -name "*.sh" -exec chmod u+x {} \;
 
-mkdir -p $HOME/packages
-cd $HOME/packages
-echo "Cloning suckless folder from github"
-git clone https://github.com/marcocappelloni/suckless.git
+if [ ! -d "$HOME/packages/suckless" ]; then
+  mkdir -p $HOME/packages
+  cd $HOME/packages
+  echo "Cloning suckless folder from github"
+  git clone https://github.com/marcocappelloni/suckless.git
+fi
 
-cd $HOME
-echo "Cloning dotfiles folder from github"
-git clone https://github.com/marcocappelloni/dotfiles.git
+if [ ! -d "$HOME/dotfiles" ]; then
+  cd $HOME
+  echo "Cloning dotfiles folder from github"
+  git clone https://github.com/marcocappelloni/dotfiles.git
+fi
