860801e85427d63480967d9154ed33f1aa64a5c9
marcocappelloni <marco.cappelloni@gmail.com>
2026-01-02 17:24:24 +0000

    Changes to the script to organize the dotfiles inside the dotfiles directory

--- debian/setup/move_dotfiles.sh
+++ debian/setup/move_dotfiles.sh
@@ -16,6 +16,13 @@ for app in "${apps[@]}"; do
   mv .config/$app $app/.config/
 done
 
-mkdir -p picom redshift
+mkdir -p picom/.config redshift/.config
 mv .config/picom.conf picom/.config/picom.conf
 mv .config/redshift.conf redshift/.config/redshift.conf
+
+stowpackages=("system" "extra" "dwm" "alacritty" "bash" "bat" "fastfetch" "ghostty" "helix" "i3" "kitty" "nvim" "polybar" "qtile" "rofi" "rofi_list" "yazi")
+
+for package in "${stowpackages[@]}"; do
+  stow -D "$package" # Unstow first to avoid old link issues
+  stow "$package"
+done
